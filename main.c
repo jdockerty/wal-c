04290f83428c9fc074c78567ec18780253381942
Jack Dockerty <jdockerty19@gmail.com>
2024-11-04 19:21:39 +0000

    feat: wip parse input kv pairs

--- main.c
+++ main.c
@@ -1,12 +1,34 @@
 #include "wal.h"
+#include <_string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//char* parse_key(char* input) {}
+//
+//char* parse_value(char* input) {}
+
+void parse_input(SegmentEntry* entries, char* input) {
+    char* buf;
+    SegmentEntry entry;
+    char* token;
+
+    // Iterate over the input string until there are no more delimited tokens
+    // remaining.
+    while (( (token = strsep(&input, ",")) != NULL)) {
+        int i;
+        // The values are delimited via '=', so we must iterate over the returned
+        // token twice.
+        for (i=0; i<2; i++) {
+            char* kv = strsep(&token, "=");
+            printf("%s\n", kv);
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc == 1) {
-        printf("No command given\n");
+        fprintf(stderr, "No command given\n");
         exit(1);
     }
 
@@ -22,13 +44,33 @@ int main(int argc, char *argv[]) {
 
         FILE* file = fopen(wal_path, "w+");
         if (file != NULL) {
-            printf("Created %s\n", wal_path);
+            fprintf(stderr, "Created %s\n", wal_path);
         } else {
             fprintf(stderr, "Unable to create WAL file.\n");
             exit(1);
         }
     } else if (strcmp(subcmd, "write") == 0) {
-        printf("write!");
+        char* wal_file_path = argv[2];
+        if (wal_file_path == NULL) {
+            fprintf(stderr, "Must provide a path to a WAL file");
+            exit(1);
+        }
+        fprintf(stderr, "Opened %s\n", wal_file_path);
+
+        char* input = argv[3];
+        if (input == NULL) {
+            fprintf(stderr, "key-value input is required.\n");
+            exit(1);
+        }
+        SegmentEntry* entries;
+        parse_input(entries, input);
+
+        // Open the file for appending at the end.
+        FILE* wal_file = fopen(wal_file_path, "a");
+        if (wal_file == NULL) {
+            fprintf(stderr, "Unable to open %s\n", wal_file_path);
+            exit(1);
+        }
     } else if (strcmp(subcmd, "replay") == 0) {
         printf("replay!");
     } else {
@@ -38,3 +80,4 @@ int main(int argc, char *argv[]) {
 
     return 0;
 }
+
