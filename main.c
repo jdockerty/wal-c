e046eefb27ffcf2cfedc09e299f4c7c505f43352
Jack Dockerty <jdockerty19@gmail.com>
2024-11-04 15:18:07 +0000

    basic file creation

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 
 int main(int argc, char *argv[]) {
@@ -11,7 +12,21 @@ int main(int argc, char *argv[]) {
 
     char* subcmd = argv[1];
     if (strcmp(subcmd, "create") == 0) {
-        printf("create!");
+        char* dir = argv[2];
+        char* wal_path;
+        asprintf(&wal_path, "%s/0.wal", dir);
+
+        FILE* file = fopen(wal_path, "w+");
+        if (file != NULL) {
+            printf("Created %s\n", wal_path);
+        } else {
+            if (errno == 14) {
+                fprintf(stderr, "No path provided\n");
+            } else {
+                fprintf(stderr, "Unable to create %s, %d\n", wal_path, errno);
+            }
+            exit(1);
+        }
     } else if (strcmp(subcmd, "write") == 0) {
         printf("write!");
     } else if (strcmp(subcmd, "replay") == 0) {
