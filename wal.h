2b0ddd12f5729b258237c3b42ea0cc4c52bc28a3
Jack Dockerty <jdockerty19@gmail.com>
2024-11-07 21:46:37 +0000

    add close subcmd

--- wal.h
+++ wal.h
@@ -19,4 +19,11 @@ bool has_header(FILE* wal_file);
 // Write the WAL_HEADER to a WAL file.
 void write_header(FILE* wal_file);
 
+// Check whether a WAL file has been closed and declared as immutable.
+bool is_closed(FILE* wal_file);
+
+// Retrieve the number of entries written to the file. This comes from the
+// encoded metadata at the footer of the file.
+int entries_metadata(FILE* wal_file);
+
 #endif
