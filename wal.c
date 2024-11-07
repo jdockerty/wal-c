4b8b842f746ab88b1cabd48b931a39ded981422d
Jack <56563911+jdockerty@users.noreply.github.com>
2024-11-07 21:02:57 +0000

    feat: encode magic bytes WAL header (#5)
    * update gitignore
    
    * header helper funcs
    
    * wire up header encoding for write/replay
    
    * docs for WAL header define
    
    * fix WAL replay seek with header included
    
    * cleanup *_header funcs

--- wal.c
+++ wal.c
@@ -1,8 +1,16 @@
 #include "wal.h"
 #include "stdio.h"
+#include <stdio.h>
+#include <string.h>
 
-char* generate_wal_path(char *dir, int id) {
-    char* path;
-    asprintf(&path, "%s/%d.wal", dir, id);
-    return path;
+bool has_header(FILE *wal_file) {
+    char header[strlen(WAL_HEADER) + 1]; // header + NULL terminator
+    fseek(wal_file, 0, SEEK_SET);
+    fread(header, sizeof(char), strlen(WAL_HEADER), wal_file);
+    header[2] = '\0';
+    return strcmp(header, WAL_HEADER) == 0;
+}
+
+void write_header(FILE *wal_file) {
+    fwrite(WAL_HEADER, sizeof(WAL_HEADER[0]), strlen(WAL_HEADER), wal_file);
 }
--- wal.h
+++ wal.h
@@ -1,16 +1,22 @@
 #ifndef WAL_H_
 #define WAL_H_
 
+#include <stdio.h>
+#include <stdbool.h>
+
+// Magic bytes that are expected at the start of a WAL file.
+#define WAL_HEADER "w1"
+
 typedef struct S {
     char* key;
     char* value;
 } SegmentEntry;
 
-typedef struct W {} Wal;
 
-// Creates a WAL path with an input directory and ID.
-char* generate_wal_path(char* dir, int id);
+// Check whether a WAL file has the expected header.
+bool has_header(FILE* wal_file);
 
-Wal new_wal(char* path);
+// Write the WAL_HEADER to a WAL file.
+void write_header(FILE* wal_file);
 
 #endif
