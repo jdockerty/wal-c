14506dbeebb1adace6a6f619bf33c100d035b710
Jack Dockerty <jdockerty19@gmail.com>
2024-11-08 14:52:12 +0000

    include style=file

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -4,5 +4,5 @@ files=$(find . -name "*.[c|h]")
 for f in $files; do
     full_path=$(realpath ${f})
     echo "${full_path}"
-    clang-format -i ${full_path}
+    clang-format -style=file -i ${full_path}
 done
